2e82ac382fd7f494f0f5a9f32b338085118e3f31
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-02 16:36:49 +0100

    hi :)

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -4,6 +4,6 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	if (s == NULL || fd < 0)
 		return ;
-	write(fd, s, strlen(s));
+	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -4,5 +4,5 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	if (s == NULL || fd < 0)
 		return ;
-	write(fd, s, strlen(s));
+	write(fd, s, ft_strlen(s));
 }
