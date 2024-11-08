bfa55d9f2a36004de5cb9c5b2fd9ccfa579fba2f
Ayman Elfadl <aelfadl@e1r4p9.1337.ma>
2024-11-08 22:13:10 +0100

    last edit inch2llah :)

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -16,6 +16,11 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 }
